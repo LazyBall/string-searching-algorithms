f1143def8123a730d73357c52305d674fd8ceeb0
Alexander <informery4@gmail.com>
2019-02-16 10:55:08 +0300

    Added test method

--- String-searching algorithms/Program.cs
+++ String-searching algorithms/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace String_searching_algorithms
 {
@@ -10,9 +11,83 @@ namespace String_searching_algorithms
     {
         static void Main()
         {
+            var str1 = "ABCABD#ABCABCAABCABD";
+            foreach(var val in Func1(str1))
+            {
+                Console.Write($"{val}| ");
+            }
+            Console.WriteLine();
+            foreach (var val in Func2(str1))
+            {
+                Console.Write($"{val}| ");
+            }
+            //var watch = new Stopwatch();
+            //for (int i = 1; i < 6; i++)
+            //{
+            //    var str = DoRandomString(10000000 * i);
+            //    watch.Start();
+            //    KnuthMorrisPrattAlgorithm.ComputePrefixFunctionHack(str);
+            //    watch.Stop();
+            //    Console.Write("Test: {0} \t Length: {1} \t Algorithm Hack: {2} ms \t", i, str.Length, watch.ElapsedMilliseconds);
+            //    watch.Reset();
+            //    watch.Start();
+            //    KnuthMorrisPrattAlgorithm.ComputePrefixFunction(str);
+            //    watch.Stop();
+            //    Console.WriteLine("Algorithm Standart: {0} ms", watch.ElapsedMilliseconds);
+            //    watch.Reset();
+            //}
 
         }
+
+        static int[] Func1(string str)
+        {
+            var pi = new int[str.Length + 1]; // значения префикс-функции + -1 в начале
+            int i = 0;
+            int j = pi[0] = -1; // благодаря -1 можно убрать лишнее условие из стандартной функции и КМП
+            while (i < str.Length)
+            {
+                while ((j > -1) && (str[i] != str[j]))
+                {
+                    j = pi[j];
+                }
+                pi[++i] = ++j;
+            }
+            return pi;
+        }
+
+        static int[] Func2(string str)
+        {
+            var pi = new int[str.Length]; // значения префикс-функции
+            pi[0] = 0; // для префикса из нуля и одного символа функция равна нулю
+            int j = 0;
+            for (int i = 1; i < str.Length; i++)
+            {
+                while ((j > 0) && (str[i] != str[j]))
+                {
+                    j = pi[j - 1];
+                }
+                if (str[i] == str[j])
+                {
+                    j++;
+                }
+                pi[i] = j;
+            }
+            return pi;
+        }
+
+        static string DoRandomString(int length)
+        {
+            var strBuilder = new StringBuilder(length);
+            var random = new Random(DateTime.Now.Millisecond);
+            for (int i = 0; i < length; i++)
+            {
+                strBuilder.Append((char)random.Next(50, 60));
+            }
+            return strBuilder.ToString();
+        }
     }
+
+  
 
     static class NaiveAlgorithm
     {
@@ -29,7 +104,7 @@ namespace String_searching_algorithms
                         break;
                     }
                 }
-                if (j == needle.Length)
+                if (j >= needle.Length)
                 {
                     return i;
                 }
@@ -51,7 +126,7 @@ namespace String_searching_algorithms
                         break;
                     }
                 }
-                if (j == needle.Length)
+                if (j >= needle.Length)
                 {
                     found = true;
                     yield return i;
