bde0bcee4c2064cb0cb1e0f12546e8046e8088a5
Alexander <informery4@gmail.com>
2019-02-21 23:16:20 +0300

    Added example of documentation

--- String-searching algorithms/Program.cs
+++ String-searching algorithms/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
@@ -11,23 +10,41 @@ namespace String_searching_algorithms
     {
         static void Main()
         {
+            int n = 100000000, l = 10;           
+            var haystack = DoRandomString(n);
+            var needle = DoRandomString(l);
 
-            //var watch = new Stopwatch();
-            //for (int i = 1; i < 6; i++)
-            //{
-            //    var str = DoRandomString(10000000 * i);
-            //    watch.Start();
-            //    KnuthMorrisPrattAlgorithm.ComputePrefixFunctionHack(str);
-            //    watch.Stop();
-            //    Console.Write("Test: {0} \t Length: {1} \t Algorithm Hack: {2} ms \t", i, str.Length, watch.ElapsedMilliseconds);
-            //    watch.Reset();
-            //    watch.Start();
-            //    KnuthMorrisPrattAlgorithm.ComputePrefixFunction(str);
-            //    watch.Stop();
-            //    Console.WriteLine("Algorithm Standart: {0} ms", watch.ElapsedMilliseconds);
-            //    watch.Reset();
-            //}
+            var watch = new Stopwatch();
 
+            watch.Start();
+            Console.WriteLine($"KMP-FindFirstEntry: {KnuthMorrisPrattAlgorithm.FindFirstEntry(needle, haystack)}");
+            watch.Stop();
+            Console.WriteLine($"ElapsedMilliseconds: {watch.ElapsedMilliseconds}");
+            watch.Reset();
+
+            watch.Start();
+            foreach(var value in KnuthMorrisPrattAlgorithm.FindAllEntries(needle, haystack))
+            {
+                Console.Write($"{value} ");
+            }
+            watch.Stop();
+            Console.WriteLine($"KMP-FindAllEntries: {watch.ElapsedMilliseconds}");
+            watch.Reset();
+
+
+            watch.Start();
+            Console.WriteLine($"NaiveAlgorithm-FindFirstEntry: {NaiveAlgorithm.FindFirstEntry(needle, haystack)}");
+            watch.Stop();
+            Console.WriteLine($"ElapsedMilliseconds: {watch.ElapsedMilliseconds}");
+            watch.Reset();
+
+            watch.Start();
+            foreach(var value in NaiveAlgorithm.FindAllEntries(needle, haystack))
+            {
+                Console.Write($"{value} ");
+            }
+            watch.Stop();
+            Console.WriteLine($"Naive-FindAllEntries: {watch.ElapsedMilliseconds}");
         }
        
         static string DoRandomString(int length)
@@ -42,11 +59,9 @@ namespace String_searching_algorithms
         }
     }
 
-
-
     static class NaiveAlgorithm
     {
-        public static int FindFirstEntry(string haystack, string needle)
+        public static int FindFirstEntry(string needle, string haystack)
         {
             int stop = haystack.Length - needle.Length + 1;
             for (int i = 0; i < stop; i++)
@@ -67,7 +82,7 @@ namespace String_searching_algorithms
             return -1;
         }
 
-        public static IEnumerable<int> FindAllEntries(string haystack, string needle)
+        public static IEnumerable<int> FindAllEntries(string needle, string haystack)
         {
             int stop = haystack.Length - needle.Length + 1;
             bool found = false;
@@ -87,7 +102,7 @@ namespace String_searching_algorithms
                     yield return i;
                 }
             }
-            if (found == false)
+            if (!found)
             {
                 yield return -1;
             }
@@ -96,7 +111,7 @@ namespace String_searching_algorithms
 
     static class KnuthMorrisPrattAlgorithm
     {
-        public static int[] ComputePrefixFunction(string str)
+        private static int[] ComputePrefixFunction(string str)
         {
             var pi = new int[str.Length]; // значения префикс-функции
             pi[0] = 0; // для префикса из нуля и одного символа функция равна нулю
@@ -116,8 +131,15 @@ namespace String_searching_algorithms
             return pi;
         }
 
-
-        public static int FindFirstEntry(string haystack, string needle)
+        /// <summary>
+        /// Ищет первое вхождение образца в данной строке.
+        /// </summary>
+        /// <returns>
+        /// Индекс первого вхождения или -1, если вхождение не найдено.
+        /// </returns>
+        /// <param name="needle">Строка, вхождение которой нужно найти.</param>
+        /// <param name="haystack">Строка, в которой осуществляется поиск.</param>
+        public static int FindFirstEntry(string needle, string haystack)
         {
             var pi = ComputePrefixFunction(needle);
             int i = 0, j = 0;
@@ -144,7 +166,8 @@ namespace String_searching_algorithms
             return -1;
         }
 
-        public static IEnumerable<int> FindAllEntries(string haystack, string needle)
+       
+        public static IEnumerable<int> FindAllEntries(string needle, string haystack)
         {
             var pi = ComputePrefixFunction(needle);
             bool found = false;
@@ -158,12 +181,18 @@ namespace String_searching_algorithms
                 if (needle[j] == haystack[i])
                 {
                     j++;
-                    if (j == needle.Length)
-                    {
-                        yield return (i - j + 1);
-                        j = pi[j - 1];
-                        found = true;
-                    }
+                    //if (j == needle.Length)
+                    //{
+                    //    yield return (i - j + 1);
+                    //    j = pi[j - 1];
+                    //    found = true;
+                    //}
+                }
+                if (j == needle.Length)
+                {
+                    yield return (i - j + 1);
+                    j = pi[j - 1];
+                    found = true;
                 }
             }
             if (!found)
