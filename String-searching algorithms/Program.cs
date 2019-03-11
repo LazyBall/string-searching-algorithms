995566db3b90e0b35427cc180afa892ad6b73138
Alexander <informery4@gmail.com>
2019-03-11 04:24:21 +0300

    Fixed Boyer-Moore algorithm

--- String-searching algorithms/Program.cs
+++ String-searching algorithms/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics;
-using System.Linq;
 
 namespace String_searching_algorithms
 {
@@ -11,50 +9,74 @@ namespace String_searching_algorithms
     {
         static void Main()
         {
-            int n = 100000000, l = 10;           
+            int n = 1000000, l = 4;
             var haystack = DoRandomString(n);
             var needle = DoRandomString(l);
 
+            //var haystack = "GCATCGCAGAGAGTATACAGTACG";
+            //var needle = "GCAGAGAG";
+
+            //Console.WriteLine(haystack);
+            //Console.WriteLine(needle);
+
             var watch = new Stopwatch();
-
-            watch.Start();
-            Console.WriteLine($"KMP-FindFirstEntry: {KnuthMorrisPrattAlgorithm.FindFirstEntry(needle, haystack)}");
-            watch.Stop();
-            Console.WriteLine($"ElapsedMilliseconds: {watch.ElapsedMilliseconds}");
-            watch.Reset();
-
-            watch.Start();
-            foreach(var value in KnuthMorrisPrattAlgorithm.FindAllEntries(needle, haystack))
-            {
-                Console.Write($"{value} ");
-            }
-            watch.Stop();
-            Console.WriteLine($"KMP-FindAllEntries: {watch.ElapsedMilliseconds}");
-            watch.Reset();
 
 
             watch.Start();
             Console.WriteLine($"NaiveAlgorithm-FindFirstEntry: {NaiveAlgorithm.FindFirstEntry(needle, haystack)}");
             watch.Stop();
             Console.WriteLine($"ElapsedMilliseconds: {watch.ElapsedMilliseconds}");
-            watch.Reset();
+            Console.WriteLine();
 
-            watch.Start();
-            foreach(var value in NaiveAlgorithm.FindAllEntries(needle, haystack))
+            watch.Restart();
+            foreach (var value in NaiveAlgorithm.FindAllEntries(needle, haystack))
             {
                 Console.Write($"{value} ");
             }
             watch.Stop();
             Console.WriteLine($"Naive-FindAllEntries: {watch.ElapsedMilliseconds}");
+            Console.WriteLine();
+
+
+            watch.Restart();
+            Console.WriteLine($"KMP-FindFirstEntry: {KnuthMorrisPrattAlgorithm.FindFirstEntry(needle, haystack)}");
+            watch.Stop();
+            Console.WriteLine($"ElapsedMilliseconds: {watch.ElapsedMilliseconds}");
+            Console.WriteLine();
+
+            watch.Restart();
+            foreach (var value in KnuthMorrisPrattAlgorithm.FindAllEntries(needle, haystack))
+            {
+                Console.Write($"{value} ");
+            }
+            watch.Stop();
+            Console.WriteLine($"KMP-FindAllEntries: {watch.ElapsedMilliseconds}");
+            Console.WriteLine();
+
+
+            watch.Restart();
+            Console.WriteLine($"BoyerMoore-FindFirstEntry: {BoyerMooreAlgorithm.FindFirstEntry(needle, haystack)}");
+            watch.Stop();
+            Console.WriteLine($"ElapsedMilliseconds: {watch.ElapsedMilliseconds}");
+            Console.WriteLine();
+
+            watch.Restart();
+            foreach (var value in BoyerMooreAlgorithm.FindAllEntries(needle, haystack))
+            {
+                Console.Write($"{value} ");
+            }
+            watch.Stop();
+            Console.WriteLine($"BoyerMoore-FindAllEntries: {watch.ElapsedMilliseconds}");
+            Console.WriteLine();
         }
-       
+
         static string DoRandomString(int length)
         {
             var strBuilder = new StringBuilder(length);
             var random = new Random(DateTime.Now.Millisecond);
             for (int i = 0; i < length; i++)
             {
-                strBuilder.Append((char)random.Next(50, 60));
+                strBuilder.Append((char)random.Next(70, 80));
             }
             return strBuilder.ToString();
         }
@@ -64,49 +86,34 @@ namespace String_searching_algorithms
     {
         public static int FindFirstEntry(string needle, string haystack)
         {
-            int stop = haystack.Length - needle.Length + 1;
-            for (int i = 0; i < stop; i++)
+
+            foreach (var element in FindAllEntries(needle, haystack))
             {
-                int j;
-                for (j = 0; j < needle.Length; j++)
-                {
-                    if (needle[j] != haystack[i + j])
-                    {
-                        break;
-                    }
-                }
-                if (j >= needle.Length)
-                {
-                    return i;
-                }
+                return element;
             }
+
             return -1;
         }
 
         public static IEnumerable<int> FindAllEntries(string needle, string haystack)
         {
             int stop = haystack.Length - needle.Length + 1;
-            bool found = false;
+
             for (int i = 0; i < stop; i++)
             {
-                int j;
-                for (j = 0; j < needle.Length; j++)
+                int j = 0;
+
+                while ((j < needle.Length) && (needle[j] == haystack[i + j]))
                 {
-                    if (needle[j] != haystack[i + j])
-                    {
-                        break;
-                    }
+                    j++;
                 }
-                if (j >= needle.Length)
+
+                if (j == needle.Length)
                 {
-                    found = true;
                     yield return i;
                 }
             }
-            if (!found)
-            {
-                yield return -1;
-            }
+
         }
     }
 
@@ -116,49 +123,29 @@ namespace String_searching_algorithms
         {
             var pi = new int[str.Length]; // значения префикс-функции
             pi[0] = 0; // для префикса из нуля и одного символа функция равна нулю
-            int j = 0;
+            int k = 0;
 
-            for (int i = 1; i < str.Length; i++)
+            for (int q = 1; q < str.Length; q++)
             {
 
-                while ((j > 0) && (str[j] != str[i]))
+                while ((k > 0) && (str[k] != str[q]))
                 {
-                    j = pi[j - 1];
+                    k = pi[k - 1];
                 }
 
-                if (str[j] == str[i])
+                if (str[k] == str[q])
                 {
-                    j++;
+                    k++;
                 }
-                pi[i] = j;
+                pi[q] = k;
             }
 
             return pi;
         }
-        
     }
 
     static class KnuthMorrisPrattAlgorithm
     {
-        public static int[] ComputePrefixFunction(string str)
-        {
-            var pi = new int[str.Length]; // значения префикс-функции
-            pi[0] = 0; // для префикса из нуля и одного символа функция равна нулю
-            int j = 0;
-            for (int i = 1; i < str.Length; i++)
-            {
-                while ((j > 0) && (str[j] != str[i]))
-                {
-                    j = pi[j - 1];
-                }
-                if (str[j] == str[i])
-                {
-                    j++;
-                }
-                pi[i] = j;
-            }
-            return pi;
-        }
 
         /// <summary>
         /// Ищет первое вхождение образца в данной строке.
@@ -170,43 +157,29 @@ namespace String_searching_algorithms
         /// <param name="haystack">Строка, в которой осуществляется поиск.</param>
         public static int FindFirstEntry(string needle, string haystack)
         {
-            var pi = ComputePrefixFunction(needle);
-            int i = 0, j = 0;
-            for (; i < haystack.Length;)
+
+            foreach (var element in FindAllEntries(needle, haystack))
             {
-                if (needle[j] == haystack[i])
-                {
-                    i++;
-                    j++;
-                    if (j == needle.Length)
-                    {
-                        return (i - j + 1);
-                    }
-                }
-                else if (j == 0)
-                {
-                    i++;
-                }
-                else
-                {
-                    j = pi[j - 1];
-                }
+                return element;
             }
+
             return -1;
         }
 
-       
+
         public static IEnumerable<int> FindAllEntries(string needle, string haystack)
         {
-            var pi = ComputePrefixFunction(needle);
-            bool found = false;
+            var pi = PrefixFunction.Compute(needle);
             int j = 0;
+
             for (int i = 0; i < haystack.Length; i++)
             {
+
                 while ((j > 0) && (needle[j] != haystack[i]))
                 {
                     j = pi[j - 1];
                 }
+
                 if (needle[j] == haystack[i])
                 {
                     j++;
@@ -221,38 +194,36 @@ namespace String_searching_algorithms
                 {
                     yield return (i - j + 1);
                     j = pi[j - 1];
-                    found = true;
                 }
             }
-            if (!found)
-            {
-                yield return -1;
-            }
+
         }
     }
 
     static class BoyerMooreAlgorithm
     {
         //Эвристика стоп-символа
-        private static IReadOnlyDictionary<char,int> ComputeLastOccurrenceFunction(string str)
+        private static IReadOnlyDictionary<char, int> ComputeLastOccurrenceFunction(string str)
         {
-            //Если алфавит маленький ASCII (256 символов) то можно сделать массив
+            //Если алфавит маленький типа ASCII (256 символов) то можно сделать массив
             // var lambda=new int[256];
             // и для каждого символа lambda[(byte)symbol]=i; , где i-самая правая позиция в строке
             // кроме последнего символа (это изменение поможет в алгоритме Хорспула)
             //В случае большого алфавита лучше использовать Dictionary
+            //В lambda нумерация символов идёт с единицы
             var lambda = new Dictionary<char, int>();
-            int m = str.Length;
-            for (int i = 0; i < m - 1; i++)
+            int m = str.Length - 1;
+
+            for (int i = 0; i < m; i++)
             {
                 char symbol = str[i];
                 if (lambda.ContainsKey(symbol))
                 {
-                    lambda[symbol] = i;
+                    lambda[symbol] = i + 1;
                 }
                 else
                 {
-                    lambda.Add(symbol, i);
+                    lambda.Add(symbol, i + 1);
                 }
             }
 
@@ -262,9 +233,9 @@ namespace String_searching_algorithms
         //Эвристика безопасного суффикса
         private static int[] ComputeGoodSuffixFunction(string str)
         {
-            var pi = KnuthMorrisPrattAlgorithm.ComputePrefixFunction(str);
-            string reverseStr = str.Reverse().ToString();
-            var piRev = KnuthMorrisPrattAlgorithm.ComputePrefixFunction(reverseStr);
+            var pi = PrefixFunction.Compute(str);
+            string reverseStr = ReverseString(str);
+            var piRev = PrefixFunction.Compute(reverseStr);
             int m = str.Length;
             // В массивах pi и piRev относительно Кормена индексы начинаются с нуля
             // при этом в gamma нумерация идет как и в книге
@@ -290,30 +261,50 @@ namespace String_searching_algorithms
 
         public static int FindFirstEntry(string needle, string haystack)
         {
+
+            foreach (var element in FindAllEntries(needle, haystack))
+            {
+                return element;
+            }
+
+            return -1;
+        }
+
+        public static IEnumerable<int> FindAllEntries(string needle, string haystack)
+        {
             int n = haystack.Length;
             int m = needle.Length;
             var lambda = ComputeLastOccurrenceFunction(needle);
             var gamma = ComputeGoodSuffixFunction(needle);
             int s = 0;
-            while(s<haystack.Length-needle.Length)
+
+            while (s < n - m + 1)
             {
                 int j = m - 1;
-                while(j>0 && needle[j]==haystack[s+j])
+                while ((j > -1) && (needle[j] == haystack[s + j]))
                 {
                     j--;
                 }
-                if (j == 0)
+                if (j == -1)
                 {
-                    return s;
+                    yield return s;
                     s += gamma[0];
                 }
                 else
                 {
                     lambda.TryGetValue(haystack[s + j], out int value);
-                    s += Math.Max(gamma[j + 1], value);
+                    value--;
+                    s += Math.Max(gamma[j + 1], j - value);
                 }
             }
-            return -1;
+
+        }
+
+        private static string ReverseString(string str)
+        {
+            char[] array = str.ToCharArray();
+            Array.Reverse(array);
+            return (new string(array));
         }
     }
 }
