68931f638e91c55913a26789a9423bdd81d7d7a7
Alexander <informery4@gmail.com>
2019-05-25 01:54:46 +0300

    Added tests

---
+++ Tests/BoyerMooreAlgorithmTests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringSearchingAlgorithms;
+
+namespace Tests
+{
+    [TestClass]
+    public class BoyerMooreAlgorithmTests
+    {
+        IStringSearchingAlgorithmTests<BoyerMooreAlgorithm> _algorithmTests =
+            new IStringSearchingAlgorithmTests<BoyerMooreAlgorithm>();
+
+        [TestMethod]
+        public void TestGetAllEntriesWhenPatternIsNull()
+        {
+            Assert.AreEqual(true, _algorithmTests.TestGetAllEntriesWhenPatternIsNull());
+        }
+
+        [TestMethod]
+        public void TestGetAllEntriesWhenTextIsNull()
+        {
+            Assert.AreEqual(true, _algorithmTests.TestGetAllEntriesWhenTextIsNull());
+        }
+
+        [TestMethod]
+        public void TestGetAllEntriesWhenPatternIsEmpty()
+        {
+            Assert.AreEqual(true, _algorithmTests.TestGetAllEntriesWhenPatternIsEmpty());
+        }
+
+        [TestMethod]
+        public void TestGetAllEntriesWhenTextIsEmptyAndPatternNotEmpty()
+        {
+            Assert.AreEqual(true, _algorithmTests.TestGetAllEntriesWhenTextIsEmptyAndPatternNotEmpty());
+        }
+
+        [TestMethod]
+        public void TestGetAllEntriesWhenPatternIsNotSubstring()
+        {
+            Assert.AreEqual(true, _algorithmTests.TestGetAllEntriesWhenPatternIsNotSubstring());
+        }
+
+        [TestMethod]
+        public void TestGetAllEntriesOnFile()
+        {
+            Assert.AreEqual(true, _algorithmTests.TestGetAllEntriesOnFile());
+        }
+
+        [TestMethod]
+        public void EasyTestGetAllEntries()
+        {
+            Assert.AreEqual(true, _algorithmTests.EasyTestGetAllEntries());
+        }
+
+        [TestMethod]
+        public void TestGetFirstEntryWhenPatternIsNull()
+        {
+            Assert.AreEqual(true, _algorithmTests.TestGetFirstEntryWhenPatternIsNull());
+        }
+
+        [TestMethod]
+        public void TestGetFirstEntryWhenTextIsNull()
+        {
+            Assert.AreEqual(true, _algorithmTests.TestGetFirstEntryWhenTextIsNull());
+        }
+
+        [TestMethod]
+        public void TestGetFirstEntryWhenPatternIsEmpty()
+        {
+            Assert.AreEqual(true, _algorithmTests.TestGetFirstEntryWhenPatternIsEmpty());
+        }
+
+        [TestMethod]
+        public void TestGetFirstEntryWhenTextIsEmptyAndPatternNotEmpty()
+        {
+            Assert.AreEqual(true, _algorithmTests.TestGetFirstEntryWhenTextIsEmptyAndPatternNotEmpty());
+        }
+
+        [TestMethod]
+        public void TestGetFirstEntryWhenPatternIsNotSubstring()
+        {
+            Assert.AreEqual(true, _algorithmTests.TestGetFirstEntryWhenPatternIsNotSubstring());
+        }
+
+        [TestMethod]
+        public void TestGetFirstEntryOnFile()
+        {
+            Assert.AreEqual(true, _algorithmTests.TestGetFirstEntryOnFile());
+        }
+
+
+        [TestMethod]
+        public void EasyTestGetFirstEntry()
+        {
+            Assert.AreEqual(true, _algorithmTests.EasyTestGetFirstEntry());
+        }
+
+    }
+}
---
+++ Tests/IStringSearchingAlgorithmTests.cs
@@ -0,0 +1,199 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using StringSearchingAlgorithms;
+
+namespace Tests
+{
+    class IStringSearchingAlgorithmTests<T> where T : IStringSearchingAlgorithm, new()
+    {
+
+        private string ReadFromFile(string fileName)
+        {
+            using (var inputFile = new StreamReader(fileName))
+            {
+                return inputFile.ReadToEnd();
+            }
+        }
+
+        private HashSet<string> DoWords(IEnumerable<char> text)
+        {
+            var strBuilder = new StringBuilder();
+            var words = new HashSet<string>();
+
+            foreach (var symbol in text)
+            {
+                if (char.IsLetter(symbol))
+                {
+                    strBuilder.Append(symbol);
+                }
+                else
+                {
+                    if (strBuilder.Length > 0)
+                    {
+                        words.Add(strBuilder.ToString());
+                        strBuilder.Clear();
+                    }
+                }
+            }
+
+            return words;
+        }
+
+        public bool TestGetAllEntriesWhenTextIsNull()
+        {
+            var flag = false;
+            try
+            {
+                new T().GetAllEntries("abc", null);
+            }
+            catch
+            {
+                flag = true;
+            }
+            return flag;
+        }
+
+        public bool TestGetAllEntriesWhenPatternIsNull()
+        {
+            var flag = false;
+            try
+            {
+                new T().GetAllEntries(null, "abc");
+            }
+            catch
+            {
+                flag = true;
+            }
+            return flag;
+        }
+
+        public bool TestGetAllEntriesWhenTextIsEmptyAndPatternNotEmpty()
+        {
+            return (new T().GetAllEntries("abc", string.Empty).Count() == 0);
+        }
+
+        public bool TestGetAllEntriesWhenPatternIsEmpty()
+        {
+            var flag = true;
+            int i = -1;
+            var text = "abc";
+
+            foreach (var item in new T().GetAllEntries(string.Empty, text))
+            {
+                i++;
+                flag = flag && (i == item);
+            }
+
+            flag = flag && (i == text.Length);
+            return flag;
+        }
+
+        public bool TestGetFirstEntryWhenTextIsNull()
+        {
+            var flag = false;
+            try
+            {
+                new T().GetFirstEntry("abc", null);
+            }
+            catch
+            {
+                flag = true;
+            }
+            return flag;
+        }
+
+        public bool TestGetFirstEntryWhenPatternIsNull()
+        {
+            var flag = false;
+            try
+            {
+                new T().GetFirstEntry(null, "abc");
+            }
+            catch
+            {
+                flag = true;
+            }
+            return flag;
+        }
+
+        public bool TestGetFirstEntryWhenTextIsEmptyAndPatternNotEmpty()
+        {
+            return (new T().GetFirstEntry("abc", string.Empty) == -1);
+        }
+
+        public bool TestGetFirstEntryWhenPatternIsEmpty()
+        {
+            var text = "abc";
+            return (new T().GetFirstEntry(string.Empty, text) == 0);
+        }
+
+        public bool EasyTestGetAllEntries()
+        {
+            var text = "abracadabra";
+            var pattern = "abr";
+            var flag = true;
+            var expected = new List<int>() { 0, 7 }.GetEnumerator();
+
+            foreach (var index in new T().GetAllEntries(pattern, text))
+            {
+                flag = flag && (index == expected.Current);
+                expected.MoveNext();
+            }
+
+            return flag;
+        }
+
+        public bool EasyTestGetFirstEntry()
+        {
+            var text = "bracadabra";
+            var pattern = "abr";
+            return (6 == new T().GetFirstEntry(pattern, text));
+        }
+
+        public bool TestGetAllEntriesWhenPatternIsNotSubstring()
+        {
+            return (new T().GetAllEntries("abcd", "efghrthyt").Count() == 0);
+        }
+
+        public bool TestGetFirstEntryWhenPatternIsNotSubstring()
+        {
+            return (new T().GetFirstEntry("abcd", "efghrthyt") == -1);
+        }
+
+        public bool TestGetAllEntriesOnFile()
+        {
+            var text = ReadFromFile("WarAndPeace.txt");
+            var flag = true;
+
+            foreach (var word in DoWords(text))
+            {
+                int i = 0;
+
+                foreach (var index in new T().GetAllEntries(word, text))
+                {
+                    flag = flag && (index == text.IndexOf(word, i));
+                    i = index + 1;
+                }
+
+            }
+
+            return flag;
+        }
+
+        public bool TestGetFirstEntryOnFile()
+        {
+            var text = ReadFromFile("WarAndPeace.txt");
+            var flag = true;
+
+            foreach (var word in DoWords(text))
+            {
+                flag = flag && (new T().GetFirstEntry(word, text) == text.IndexOf(word));
+            }
+
+            return flag;
+        }
+
+    }
+}
